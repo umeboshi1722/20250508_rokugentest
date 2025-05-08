aa04e37e1a4238e6a8cf2edbfef20abd321da6ff
Umeboshi <umeboshi1722@gmail.com>
2025-05-08 22:18:34 +0900

    branch2に出力内容を変更

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-  printf("Hello Git_branch1_1!\n");
-  printf("Hello Git_branch1_2!\n");
-  printf("Hello Git_branch1_3!\n");
+  printf("Hello Git_branch2_1!\n");
+  printf("Hello Git_branch2_2!\n");
+  printf("Hello Git_branch2_3!\n");
 
   return 0;
 }
