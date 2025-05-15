7f8c27dfbad4fec61a85548082593a4bf94f6755
umeboshi <umeboshi1722@gmail.com>
2025-05-16 00:16:38 +0900

    コメントを追加

--- main.c
+++ main.c
@@ -3,15 +3,19 @@
 
 int main()
 {
+  // 棒
   disp_stick(5);
   printf("\n");
 
+  // 三角
   disp_triangle(5);
   printf("\n");
 
+  // 逆三角
   disp_triangle_reverse(5);
   printf("\n");
 
+  // 四角
   disp_square(5);
   printf("\n");
 
